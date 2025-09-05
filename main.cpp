ff3ce7f804a20143d63b18f10b86a1e918a23fbe
Absolut Chud Logek <bazhantt@gmail.com>
2025-09-05 18:07:11 -0400

    some paths work good, on someother filter undershoots and then catches up properly?

--- main.cpp
+++ main.cpp
@@ -12,12 +12,9 @@
 
 
 int main() {
-    std::vector<std::vector<double>> data = DataReader::readDataFile("test.txt", ' ');
-    std::vector<std::vector<double>> jerkyData = DataReader::readDataFile("more-data.txt", ' ');
-    std::vector<std::vector<double>> dataActual = DataReader::readDataFile("z.txt", '\t');
-    if (data.empty()) {
-        return -1;
-    }
+    // std::vector<std::vector<double>> data = DataReader::readDataFile("test.txt", ' ');
+    std::vector<std::vector<double>> jerkyData = DataReader::readDataFile("newdata.txt", ' ');
+    // std::vector<std::vector<double>> dataActual = DataReader::readDataFile("z.txt", '\t');
 
     UKF ukf;
     Odometry odom;
@@ -27,19 +24,13 @@ int main() {
     std::vector<std::vector<double>> points;
     std::vector<std::vector<double>> points2;
 
-    auto initial = jerkyData.at(0);
-    ukf.x(0) = initial.at(0);
-    ukf.x(1) = initial.at(1);
-    ukf.x(2) = initial.at(2);
-    // ukf.x(2) = initial.at(2) + M_PI / 2.0;
-    ukf.x(3) = initial.at(0);
-    ukf.x(4) = initial.at(1);
-    ukf.x(5) = initial.at(2);
+    // odom.current = V3d { 23.25, 23.25, M_PI / 2.0 };
+    // ukf.x = V6d { 23.25, 23.25, M_PI / 2.0, 0.0, 0.0, 0.0 };
 
     // odom.update({ initial.at(0), initial.at(1), initial.at(2) + M_PI / 2.0 });
 
     int dist = 0;
-    for(size_t i = 1; i < jerkyData.size() && i < 900; i++) {
+    for(size_t i = 1; i < jerkyData.size() && i < 11200; i++) {
         auto p = jerkyData.at(i);
         double dx = p.at(0);
         double dy = p.at(1);
@@ -56,10 +47,12 @@ int main() {
         // std::cout << std::endl;
         // formatMatrix(o.transpose());
         // formatMatrix(ukf.x.transpose());
-        if(dist++ % 8 == 0) {
+        if(dist++ % 4 == 0) {
             savePointData(points, points2, o, ukf.x);
         }
+
     }
+
 
     std::cout << "\n\nEnd: " << std::endl;
     formatMatrix(odom.current.transpose());
